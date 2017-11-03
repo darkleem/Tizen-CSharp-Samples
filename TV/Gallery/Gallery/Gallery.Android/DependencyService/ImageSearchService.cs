603c5c855151b1605f1a6c8081716cb3dee69e5a
darkleem <cdark.lim@samsung.com>
2017-11-03 18:49:22 +0900

    Add Gallery sample

---
+++ TV/Gallery/Gallery/Gallery.Android/DependencyService/ImageSearchService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Android.Text;
+using Gallery.Droid.DependencyService;
+using Xamarin.Forms;
+using static Android.Provider.MediaStore;
+
+[assembly: Dependency(typeof(ImageSearchService))]
+namespace Gallery.Droid.DependencyService
+{
+    public class ImageSearchService : IImageSearchService
+    {
+        public IList<string> GetImagePathsAsync()
+        {
+            var result = new List<string>();
+            var uri = Android.Provider.MediaStore.Images.Media.ExternalContentUri;
+
+            string[] projection = { MediaColumns.Data, MediaColumns.DisplayName };
+
+            var cursor = Android.App.Application.Context.ContentResolver.Query(uri, projection, null, null, MediaColumns.DateAdded + " desc");
+            int columnIndex = cursor.GetColumnIndexOrThrow(MediaColumns.Data);
+            int columnDisplayname = cursor.GetColumnIndexOrThrow(MediaColumns.DisplayName);
+
+            int lastIndex;
+            while (cursor.MoveToNext())
+            {
+                String absolutePathOfImage = cursor.GetString(columnIndex);
+                String nameOfFile = cursor.GetString(columnDisplayname);
+                lastIndex = absolutePathOfImage.LastIndexOf(nameOfFile);
+                lastIndex = lastIndex >= 0 ? lastIndex : nameOfFile.Length - 1;
+
+                if (!TextUtils.IsEmpty(absolutePathOfImage))
+                {
+                    result.Add(absolutePathOfImage);
+                }
+            }
+
+            foreach (var str in result)
+            {
+                Debug.WriteLine($"@@@@PhotoSelectActivity.java | getPathOfAllImages | : {str}");
+            }
+            return result;
+        }
+    }
+}
---
+++ TV/Gallery/Gallery/Gallery.UWP/DependencyService/ImageSearchService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Gallery.UWP.DependencyService;
+using Windows.ApplicationModel;
+using Windows.Storage;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ImageSearchService))]
+namespace Gallery.UWP.DependencyService
+{
+    public class ImageSearchService : IImageSearchService
+    {
+        public IList<string>  GetImagePathsAsync()
+        {
+            var filters = new string[] { "jpg", "jpeg", "png", "gif", "tiff", "bmp" };
+            List<FileInfo> filesFound = new List<FileInfo>();
+            var searchOption = SearchOption.TopDirectoryOnly;
+
+            DirectoryInfo info = new DirectoryInfo(Package.Current.InstalledLocation.Path);
+
+            foreach (var filter in filters)
+            {
+                filesFound.AddRange(info.GetFiles(string.Format("*.{0}", filter), searchOption));
+            }
+
+            var result = filesFound.Select((i, s) => i.Name).ToList();
+            result.Sort();
+
+            return result;
+        }
+    }
+}
